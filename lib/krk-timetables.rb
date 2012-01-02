18fce7c825547c69e9aafb8913b684e8d8c44dd8
Marek Nowak <mareknowakk@gmail.com>
2012-01-02 21:39:00 +0100

    initial commit

---
+++ lib/krk-timetables.rb
@@ -0,0 +1,7 @@
+require "krk-timetables/version"
+
+module Krk
+  module Timetables
+    # Your code goes here...
+  end
+end
---
+++ lib/krk-timetables/version.rb
@@ -0,0 +1,5 @@
+module Krk
+  module Timetables
+    VERSION = "0.0.1"
+  end
+end
